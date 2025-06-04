394a591c409d17c722bb991f17799ca9f6d92b97
smsalim931 <smsalim931@gmail.com>
2025-06-04 22:03:12 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,49 @@ const router = express.Router()
 const todos = [{
     id : '1',
     task : 'buy milk',
+},
+{
+    id : '2',
+    task : 'buy eggs'
 }]
 
 router.get('/todo', (req, res)=>{
     res.render('index', {todos})
 })
+
+router.get('/newTodo', (req, res)=>{
+    res.render('new')
+})
+
+router.post('/newTodo', (req, res)=>{
+    const newTask = req.body.task
+    if(newTask){
+        const newTodo = {
+            id : todos.length + 1,
+            task : newTask
+        }
+        todos.push(newTodo)
+    }
+    res.redirect('/todo')
+})
+
+router.get('/edit/:id', (req, res)=>{
+    const id = req.params.id
+    const todo = todos.filter(todo => todo.id === id)
+    res.render('edit', {todo: todo[0]})
+
+    
+})
+
+router.post('/edit/:id', (req, res)=>{
+    const id = req.params.id
+    const newTask = req.body.todo
+    const todoIndex = todos.findIndex(todo => todo.id === id)
+    if(todoIndex !== -1 && newTask){
+        todos[todoIndex].task = newTask
+    }
+    res.redirect('/todo')
+})
+
 
 module.exports = router
